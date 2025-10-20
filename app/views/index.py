ec2ed4d7b6f733b397f6dd85f7b60b48da32faea
Steven A Moore <stevenallenmoore@gmail.com>
2025-10-20 19:54:51 +0000

    More Foundation conversions

--- app/views/index.py
+++ app/views/index.py
@@ -129,13 +129,12 @@ def api(rest_path):
             params = dict(request.json)
         params["response_path"] = response_url
         params |= {"user": str(AutoAuth.current_user().pk)}
-        log("API POST REQUEST", rest_path, params)
-        if "/admin/" in url:  # and user.is_admin:
-            response = requests.post(url, json=params).text
-        elif params.get("model") and params.get("pk"):
+        if params.get("model") and params.get("pk"):
             obj = AutoModel.get_model(params.get("model"), params.get("pk"))
             if _authenticate(user, obj.world):
                 response = requests.post(url, json=params).text
+        else:
+            response = requests.post(url, json=params).text
     # log(response)
     return response
 
