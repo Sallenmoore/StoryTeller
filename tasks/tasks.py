39f413298dd997fa0a56820c02e19e5ff98eacb5
Steven A Moore <stevenallenmoore@gmail.com>
2025-11-16 19:14:34 +0000

    updated

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -58,7 +58,7 @@ def _generate_audio_transcription_task(model, pk):
     if obj := AutoModel.get_model(model, pk):
         transcription = Audio.transcribe(
             obj.audio,
-            prompt="Please provide a detailed transcription of the audio recording from a TTRPG Session. Seperate different speakers if applicable. Leave out any 'umms' or 'ahs' or similar filler words. Focus on the narrative and events rather than small talk and game mechanics.",
+            prompt="Please provide a detailed overview of the audio recording from a TTRPG Session. Seperate different speakers as much as possible. Leave out any 'umms' or 'ahs' or similar filler words. Focus on the narrative and events rather than small talk and game mechanics.",
         )
         obj.transcription += f"""
 <br><br>
