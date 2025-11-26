2dd97e23bf70059788e5b1e392c3ecb0574aa78d
Steven A Moore <stevenallenmoore@gmail.com>
2025-11-26 19:15:20 +0000

    updated

--- app/views/admin.py
+++ app/views/admin.py
@@ -34,26 +34,32 @@ def index():
 @admin_page.route("/manage/migrate", methods=("POST",))
 # @auth_required(admin=True)
 def migrate():
-    # worlds = World.all()
-    # for world in worlds:
-    #     objs = [
-    #         *Character.search(world=world),
-    #         *Creature.search(world=world),
-    #         *Item.search(world=world),
-    #         *Vehicle.search(world=world),
-    #     ]
-    #     for obj in objs:
-    #         for a in obj.abilities:
-    #             if not a.world or a.world != world:
-    #                 a.world = world
-    #                 a.save()
-    #                 log(f"Updated Ability {a.name} for {world.name}")
+    worlds = World.all()
+    for world in worlds:
+        for a in world.abilities:
+            abilities = Ability.search(name=a.name)
+            for dup_a in abilities:
+                if dup_a.pk != a.pk:
+                    a.description = dup_a.description or a.description
+                    a.action = dup_a.action or a.action
+                    a.category = dup_a.category or a.category
+                    a.effects = dup_a.effects or a.effects
+                    a.duration = dup_a.duration or a.duration
+                    a.dice_roll = dup_a.dice_roll or a.dice_roll
+                    a.mechanics = dup_a.mechanics or a.mechanics
+                    a.save()
+                    log(f"Deleted duplicate Ability {dup_a.name} in world {world.name}")
+                    dup_a.delete()
+            if not a.world or a.world != world:
+                a.world = world
+                a.save()
+                log(f"Updated Ability {a.name} for {world.name}")
 
-    # orphan_abilities = Ability.search(world=None)
-    # log(f"Found {len(orphan_abilities)} orphan abilities")
-    # for a in orphan_abilities:
-    #     log(
-    #         f"Deleted orphan ability {a.name} with world {a.world.name if a.world else 'None'} and description: \n{a.description}"
-    #     )
-    #     a.delete()
+    orphan_abilities = Ability.search(world=None)
+    log(f"Found {len(orphan_abilities)} orphan abilities")
+    for a in orphan_abilities:
+        log(
+            f"Deleted orphan ability {a.name} with world {a.world.name if a.world else 'None'} and description: \n{a.description}"
+        )
+        a.delete()
     return "success"
