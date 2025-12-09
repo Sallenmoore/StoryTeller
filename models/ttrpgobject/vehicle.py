7e7f49aeb498a514468aa72528cb8dc351bfb9e7
Steven A Moore <stevenallenmoore@gmail.com>
2025-12-09 22:22:45 +0000

    updated

--- models/ttrpgobject/vehicle.py
+++ models/ttrpgobject/vehicle.py
@@ -89,54 +89,6 @@ class Vehicle(Place):
                     "items": {"type": "string"},
                     "description": "A concise list of significant events that have recently occurred in this vehicle, even if they aren't ongoing situations. Only include publicly known information.",
                 },
-                "abilities": {
-                    "type": "array",
-                    "description": "Generate at least 2 offensive combat, 2 defensive combat AND 2 roleplay special ability objects for the array. Each object in the array should have attributes for the ability name, type of action, detailed description in MARKDOWN, effects, duration, and the dice roll mechanics involved in using the ability.",
-                    "items": {
-                        "type": "object",
-                        "required": [
-                            "name",
-                            "action",
-                            "description",
-                            "effects",
-                            "duration",
-                            "dice_roll",
-                        ],
-                        "properties": {
-                            "name": {
-                                "type": "string",
-                                "description": "Unique name for the Ability.",
-                            },
-                            "action": {
-                                "type": "string",
-                                "enum": [
-                                    "main action",
-                                    "bonus action",
-                                    "reaction",
-                                    "free action",
-                                    "passive",
-                                ],
-                                "description": "type of action required.",
-                            },
-                            "description": {
-                                "type": "string",
-                                "description": "Detailed description of the ability and how the Vehicle implements the ability in MARKDOWN.",
-                            },
-                            "effects": {
-                                "type": "string",
-                                "description": "Description of the ability's effects.",
-                            },
-                            "duration": {
-                                "type": "string",
-                                "description": "The duration of the ability's effects.",
-                            },
-                            "dice_roll": {
-                                "type": "string",
-                                "description": "The dice roll mechanics for determining the success or failure of the ability.",
-                            },
-                        },
-                    },
-                },
             },
         },
     }
@@ -162,9 +114,16 @@ class Vehicle(Place):
 
     ################### CRUD Methods #####################
     def generate(self):
-        prompt = f"""Create a {random.choice(["highly advanced", "dilapidated", "warclad", "commercial", "opulent"])} {self.genre} {self.type} that has a {random.choice(("unexpected", "mysterious", "sinister", "incredible"))} history.
+        prompt = f"""Create a {self.genre} {self.type} {self.make} that has a capacity of {self.capacity}.
         """
-        return super().generate(prompt=prompt)
+        super().generate(prompt=prompt)
+
+        for _ in range(random.randint(1, 6)):
+            ability = Ability(world=self.world)
+            ability.save()
+            self.abilities.append(ability)
+            self.save()
+            ability.generate(self)
 
     ################### Instance Methods #####################
 
@@ -212,7 +171,7 @@ class Vehicle(Place):
     def auto_pre_save(cls, sender, document, **kwargs):
         super().auto_pre_save(sender, document, **kwargs)
         document.pre_save_size()
-        document.pre_save_ability()
+        # document.pre_save_ability()
 
     # @classmethod
     # def auto_post_save(cls, sender, document, **kwargs):
@@ -235,21 +194,21 @@ class Vehicle(Place):
             log(f"Invalid size for vehicle: {self.size}", _print=True)
             self.size = "medium"
 
-    def pre_save_ability(self):
-        for idx, ability in enumerate(self.abilities):
-            if isinstance(ability, str):
-                a = Ability(description=ability)
-                a.save()
-                self.abilities[idx] = a
-            elif isinstance(ability, dict):
-                a = Ability(**ability)
-                a.save()
-                self.abilities[idx] = a
-            else:
-                ability.description = (
-                    markdown.markdown(ability.description.replace("```markdown", ""))
-                    .replace("h1>", "h3>")
-                    .replace("h2>", "h3>")
-                )
+    # def pre_save_ability(self):
+    #     for idx, ability in enumerate(self.abilities):
+    #         if isinstance(ability, str):
+    #             a = Ability(description=ability)
+    #             a.save()
+    #             self.abilities[idx] = a
+    #         elif isinstance(ability, dict):
+    #             a = Ability(**ability)
+    #             a.save()
+    #             self.abilities[idx] = a
+    #         else:
+    #             ability.description = (
+    #                 markdown.markdown(ability.description.replace("```markdown", ""))
+    #                 .replace("h1>", "h3>")
+    #                 .replace("h2>", "h3>")
+    #             )
 
-        self.abilities = [a for a in self.abilities if a.name]
+    #     self.abilities = [a for a in self.abilities if a.name]
